940d7db1d1e682b852e2842fe75e4d9dcaf9f6d1
Bandarof <bandarof@gmail.com>
2025-12-25 14:14:15 +0100

    Update

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { BuilderComponent } from "@builder.io/react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello Builder</h1>
+      <BuilderComponent model="page" />
+    </div>
+  );
+}
